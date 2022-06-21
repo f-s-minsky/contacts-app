b6db31bcddc662f7dafd52b372a78160e6c5a1e8
f-s-minsky <f.s.minsky@gmail.com>
2022-06-21 18:02:53 +0200

    ft: db connection

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 import authUser from './routes/auth.js';
 import contacts from './routes/contacts.js';
 import users from './routes/users.js';
 
+// Load .env file content (env variables) in process.env
+dotenv.config({ path: './config/config.env' });
+
+// Connect Database
+connectDB();
+
 const app = express();
+
+// Init Middleware for parsing reques body
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Hello from get /' });
@@ -17,5 +28,7 @@ app.use('/api/contacts', contacts);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  );
 });
