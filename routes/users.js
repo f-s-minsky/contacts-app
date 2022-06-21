3533ebc041d3c5db5ee83eff2b4aea72618ff505
f-s-minsky <f.s.minsky@gmail.com>
2022-06-21 19:10:14 +0200

    feat: hash password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+
 const router = express.Router();
 
 import User from '../models/User.js';
@@ -11,7 +13,7 @@ import User from '../models/User.js';
 // @access  Public
 router.post(
   '/',
-  // [check('name', 'name is required').not().isEmpty()],
+  // req validation
   [
     body('name', 'Please add name').not().isEmpty(),
     body('email', 'Please include a valid email').isEmail(),
@@ -20,14 +22,47 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res
         .status(400)
         .json({ errors: errors.array() });
     }
-    res.json('passed');
+    // res after validation ok
+    const { name, email, password } = req.body;
+
+    // test if user already exist
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: 'User already exists' });
+      }
+
+      // Create new user but not save
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Password hash generation
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save new user in DB
+      await user.save();
+
+      // For testing
+      res.json('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json('Server Error');
+    }
   }
 );
 
