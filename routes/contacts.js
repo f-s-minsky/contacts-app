84da115895f2a84b88683d9d2e2f9f2e85e13e09
f-s-minsky <f.s.minsky@gmail.com>
2022-06-23 09:54:34 +0200

    feat: delete contact route

--- routes/contacts.js
+++ routes/contacts.js
@@ -20,7 +20,7 @@ router.get('/', auth, async (req, res) => {
     res.json(contacts);
   } catch (err) {
     console.error(err.message);
-    res.status(500).json('Server Error');
+    res.status(500).json({ msg: 'Server Error' });
   }
 });
 // @route   POST api/contacts
@@ -60,7 +60,7 @@ router.post(
       res.json(contact);
     } catch (err) {
       console.error(err.message);
-      res.status(500).json('Server Error');
+      res.status(500).json({ msg: 'Server Error' });
     }
   }
 );
@@ -82,6 +82,7 @@ router.put('/:id', auth, async (req, res) => {
   try {
     let contact = await Contact.findById(req.params.id);
 
+    // console.log('contact', contact);
     if (!contact)
       return res
         .status(404)
@@ -105,15 +106,35 @@ router.put('/:id', auth, async (req, res) => {
     res.json(contact);
   } catch (err) {
     console.error(err.message);
-    res.status(500).json('Server Error');
+    res.status(500).json({ msg: 'Server Error' });
   }
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
-router.delete('/:id', (req, res) => {
-  res.json({ msg: 'Delete contact' });
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    let contact = await Contact.findById(req.params.id);
+    if (!contact)
+      return res
+        .status(404)
+        .json({ msg: 'Contact not found' });
+
+    // Check if user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res
+        .status(401)
+        .json({ msg: 'Not authorized' });
+    }
+
+    await Contact.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: ' Contact removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
 });
 
 export default router;
