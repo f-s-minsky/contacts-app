da647024e86d84048ca4dce197aa04cc48434293
f-s-minsky <f.s.minsky@gmail.com>
2022-06-27 11:15:12 +0200

    feat: contactForm component

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react';
-// import {v4 as uuidv4} from 'uuid'
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
+// import uuid from 'uuid'; Brad version
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -46,6 +46,10 @@ const ContactState = (props) => {
   );
 
   // Add Contact
+  const addContact = (contact) => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
 
@@ -61,7 +65,10 @@ const ContactState = (props) => {
 
   return (
     <ContactContext.Provider
-      value={{ contacts: state.contacts }}
+      value={{
+        contacts: state.contacts,
+        addContact,
+      }}
     >
       {props.children}
     </ContactContext.Provider>
---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,24 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  CLEAR_FILTER,
+} from '../types';
+
+const contactReducer = (state, action) => {
+  // export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default contactReducer;
